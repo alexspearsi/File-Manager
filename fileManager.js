7ff81dfe1fd092e47d1f304f5784bf78f47a4f26
Alex <strelchenko.aleksandr@yandex.ru>
2025-11-03 11:44:45 +0200

    feat: add hash function

--- fileManager.js
+++ fileManager.js
@@ -1,4 +1,4 @@
-import { cd, ls, up } from './utils/index.js';
+import { cd, ls, up, hash } from './utils/index.js';
 
 export async function fileManager(input, currentDir) {
   const [command, ...args] = input.split(' ');
@@ -14,8 +14,14 @@ export async function fileManager(input, currentDir) {
       await ls(currentDir);
       return currentDir;
 
+    case 'hash':
+      await hash(currentDir, args[0]);
+      return currentDir;
+
     default:
       console.log('No such option');
       return currentDir;
   }
 }
+
+
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,4 @@
 export { cd } from './cd.js';
 export { ls } from './ls.js';
 export { up } from './up.js';
+export { hash } from './hash.js';
