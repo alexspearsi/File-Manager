01a580a8e72cc31d2a9090557459d907d861e1d7
Alex <strelchenko.aleksandr@yandex.ru>
2025-11-03 08:30:21 +0200

    feat: add goodbye message on application exit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import os from 'node:os';
+import readline from 'node:readline'
+
 const args = process.argv;
 const usernameArg = args.find(arg => arg.startsWith('--username='))
 
@@ -8,3 +11,24 @@ const user = rawUser.length > 0
   : "Anonymous";
 
 console.log(`Welcome to the File Manager, ${user}`);
+
+const currentDir = os.homedir();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: `You are currently in ${currentDir}\n`
+})
+
+rl.prompt();
+
+rl.on('line', async(input) => {
+
+})
+
+rl.on('close', () => {
+  console.log(`Thank you for using File Manager, ${user}, goodbye!`);
+  process.exit(0);
+})
+
+process.on("SIGINT", () => rl.close())
